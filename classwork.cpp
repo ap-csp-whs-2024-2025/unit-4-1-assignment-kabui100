faf628b1900848308e8df1642bbc090953b47f15
kabui100 <andybuibui2020@gmail.com>
2024-12-11 21:04:20 +0000

    first day ZAZA 4.1

--- classwork.cpp
+++ classwork.cpp
@@ -3,7 +3,13 @@
 
 int main()
 {
-  // write code here
-  
-  return 0;
+  int age = 16; // SET age TO 16
+  double price = 19.99;
+  bool isRaining = false;
+
+  std::cout << age << "\n" << price << std::endl << isRaining <<"\n";
+
+  int grade_level = 11;
+  std::cout<<"I am an " << grade_level << "th grader." << std::endl
+  ;return 0;
 }
